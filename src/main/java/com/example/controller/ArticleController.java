79864fc69a0c5c676df07aabf28c0b6b59663378
kazuaki-aso-rp <kazuaki.aso@rakus-partners.co.jp>
2022-02-15 16:34:35 +0900

    記事のみ投稿完了

--- src/main/java/com/example/controller/ArticleController.java
+++ src/main/java/com/example/controller/ArticleController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.Article;
+import com.example.form.ArticleForm;
 import com.example.service.ArticleService;
 
 @Controller
@@ -18,11 +20,26 @@ public class ArticleController {
 	private ArticleService articleService;
 
 	@RequestMapping("")
-	private String index(Model model) {
+	public String index(Model model) {
 		List<Article> articleList = articleService.findAll();
 		model.addAttribute("articleList", articleList);
-		
+
 		return "bbs";
+	}
+
+	@ModelAttribute
+	private ArticleForm setUpForm() {
+		return new ArticleForm();
+	}
+
+	@RequestMapping("/insert")
+	public String insertArticle(ArticleForm form, Model model) {
+		Article article = new Article();
+		article.setName(form.getName());
+		article.setContent(form.getContent());
+		articleService.insert(article);
+		return index(model);
+
 	}
 
 }
